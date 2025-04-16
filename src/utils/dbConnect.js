b4111f25ef15c220574a8a8945c93f9702258086
Sreeram <164004233+sreeramsmfai@users.noreply.github.com>
2025-04-16 07:35:27 +0530

    Update dbConnect.js

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = "mongodb://sreeramsdev:Sree2505@#@cluster0.zemya.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const MONGODB_URI = process.env.MONGODB_URI;
 if (!MONGODB_URI) {
   throw new Error('❌ MONGODB_URI is not defined in environment variables');
 }
