6e9034bb7a1dd2f7a4ac9995353b997557696251
Sreeram <164004233+sreeramsmfai@users.noreply.github.com>
2025-04-16 07:57:46 +0530

    Update dbConnect.js

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
-console.log("MONGODB_URI:", MONGODB_URI);
-if (!MONGODB_URI) {
+const URI = process.env.MONGODB_URI;
+console.log("MONGODB_URI:", URI);
+/*if (!URI) {
   throw new Error('❌ MONGODB_URI is not defined in environment variables');
-}
-console.log("🚀 Mongo URI:", MONGODB_URI);
+}*/
+console.log("🚀 Mongo URI:", URI);
 let cached = global.mongoose;
 
 if (!cached) {
@@ -16,7 +16,7 @@ async function dbConnect() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
+    cached.promise = mongoose.connect(URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }).then((mongoose) => mongoose);
